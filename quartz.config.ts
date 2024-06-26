db5b170195a59eef330f9fe705c5d3250d9c3271
greenned <meetingstranger@gmail.com>
2024-06-26 18:03:08 +0900

    Quartz sync: Jun 26, 2024, 6:03 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Greenned's Garden",
+    pageTitle: "greenned",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
